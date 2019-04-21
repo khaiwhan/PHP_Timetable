b88ab736e1fd3edf8b69e30a71b48c83081fb1d8
Khaiwhan <sritonchai.p@gmail.com>
2019-04-22 02:59:42 +0700

    create login component

---
+++ Views/Login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Timetable</title>
+    <link rel="stylesheet" href="../../Styles/Login/Login.css">
+</head>
+
+<body>
+    <div class="grid-container">
+        <div class="grid-item"></div>
+        <div class="grid-item">
+            <form action="#" method="post" class="form-layout">
+                <div>
+                    <img src="../../Shared/Icon/schedule.svg" alt="">
+                    <h1> Timetable Management System</h1>
+                    <input type="text" name="username" id="username" placeholder="Username">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    <button type="submit">Sign-in</button>
+                </div>
+            </form>
+        </div>
+        <div class="grid-item"></div>
+    </div>
+</body>
+
+</html>
